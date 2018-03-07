e2a30e22f68135511e209d6ed5d7ebfc3f21a137
chenpingfiona <chen_ping2010@hotmail.com>
2018-03-07 23:57:32 +0800

    add in exception and quiz title list adapter

---
+++ app/src/main/java/nus/edu/sg/photolearnse26pt03/model/AppException.java
@@ -0,0 +1,12 @@
+package nus.edu.sg.photolearnse26pt03.model;
+
+/**
+ * Created by chen ping on 7/3/2018.
+ */
+
+public class AppException extends Exception{
+
+    public AppException (String msg) {
+        super (msg);
+    }
+}
