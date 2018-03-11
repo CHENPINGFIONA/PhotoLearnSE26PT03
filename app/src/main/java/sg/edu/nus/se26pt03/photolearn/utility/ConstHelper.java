b1ab96ca7da3c45ae0d1eced73888f9361e1d9f0
wyj5230 <e0000371@u.nus.edu>
2018-03-11 22:41:29 +0800

    add DB connection consts into constHelper

--- app/src/main/java/sg/edu/nus/se26pt03/photolearn/utility/ConstHelper.java
+++ app/src/main/java/sg/edu/nus/se26pt03/photolearn/utility/ConstHelper.java
@@ -6,4 +6,17 @@ package sg.edu.nus.se26pt03.photolearn.utility;
 
 public class ConstHelper {
     public static String SharedPreferences_User_Id = "USERID";
+
+    //these consts are for DB connection, please do not modify
+    //start of DB const
+    public static String LearningItem = "LearningItem";
+    public static String LearningTitle = "LearningTitle";
+    public static String LearningSession = "LearningSession";
+    public static String QuizTitle = "QuizTitle";
+    public static String QuizOption = "QuizOption";
+    public static String QuizItem = "QuizItem";
+    public static String QuizAnswer = "QuizAnswer";
+    public static String QuizAnswerOption = "QuizAnswerOption";
+    public static String User = "User";
+    //end of DB const
 }
