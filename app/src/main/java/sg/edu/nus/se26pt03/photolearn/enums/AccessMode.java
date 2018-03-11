4692701e9ed54a9b98f2895f3057116b66f8d55f
chenpingfiona <chen_ping2010@hotmail.com>
2018-03-11 17:45:27 +0800

    map accessmode enum to int

--- app/src/main/java/sg/edu/nus/se26pt03/photolearn/enums/AccessMode.java
+++ app/src/main/java/sg/edu/nus/se26pt03/photolearn/enums/AccessMode.java
@@ -6,6 +6,20 @@ package sg.edu.nus.se26pt03.photolearn.enums;
 
 public enum AccessMode {
     EDIT,
-    VIEW
+    VIEW;
+
+    public static AccessMode fromInt(int x) {
+        return AccessMode.values()[x];
+    }
+
+    public static int toInt(AccessMode mode) {
+        switch (mode) {
+            case VIEW:
+                return 0;
+            case EDIT:
+                return 1;
+        }
+        return 0;
+    }
 }
 
