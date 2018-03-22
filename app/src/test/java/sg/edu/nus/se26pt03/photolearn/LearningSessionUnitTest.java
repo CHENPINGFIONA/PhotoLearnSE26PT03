76a2fdf2a39bc06cf2d624b6de53c2dc88b71213
chenpingfiona <chen_ping2010@hotmail.com>
2018-03-23 00:24:41 +0800

    learning session unit test

--- app/src/test/java/sg/edu/nus/se26pt03/photolearn/LearningSessionUnitTest.java
+++ app/src/test/java/sg/edu/nus/se26pt03/photolearn/LearningSessionUnitTest.java
@@ -1,7 +1,11 @@
 package sg.edu.nus.se26pt03.photolearn;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
 import sg.edu.nus.se26pt03.photolearn.BAL.LearningSession;
 import sg.edu.nus.se26pt03.photolearn.BAL.LearningTitle;
 import sg.edu.nus.se26pt03.photolearn.BAL.QuizTitle;
@@ -11,21 +15,50 @@ import sg.edu.nus.se26pt03.photolearn.BAL.QuizTitle;
  */
 
 public class LearningSessionUnitTest {
-    private LearningSession session=null;
+    private LearningSession session = null;
     private List<LearningTitle> learningTitles;
     private List<QuizTitle> quizTitles;
 
     public LearningSessionUnitTest() {
-        SetInitialValues();
+        setInitialValues();
     }
 
-    private void SetInitialValues() {
+    private void setInitialValues() {
+        session = new LearningSession();
+        session.setId("1");
+        session.setCourseCode("cs");
+        session.setCourseDate(new Date());
+        session.setCourseName("Course 1");
+        session.setModuleName("md");
+        session.setModuleNumber(1);
+        session.setCreatedBy("chen ping");
+        session.setTimestamp(new Date());
 
+        learningTitles = new ArrayList<>();
+        LearningTitle title = new LearningTitle();
+        title.setLearningSession(session);
+        title.setTitle("learning title 1");
+        title.setCreatedBy("chen ping");
+        title.setTimestamp(new Date());
+
+        learningTitles.add(title);
+
+        title.setTitle("learning title 2");
+        title.setCreatedBy("chen ping");
+        title.setTimestamp(new Date());
+
+        learningTitles.add(title);
     }
 
     @Test
     public void Assert_CreateLearningTitle() throws Exception {
+        LearningTitle title = new LearningTitle();
+        title.setLearningSession(session);
+        title.setTitle("learning title 3");
+        title.setCreatedBy("chen ping");
+        title.setTimestamp(new Date());
 
+        session.addLearningTitle(title);
     }
 
     @Test
