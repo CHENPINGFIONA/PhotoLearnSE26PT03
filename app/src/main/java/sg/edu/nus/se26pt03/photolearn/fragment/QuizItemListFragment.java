06fcd485296bc21e0cc0ff1e44f440ce6ea3b4f2
bkdpchaturvedi <37078558+bkdpchaturvedi@users.noreply.github.com>
2018-03-27 01:46:55 +0800

    QUiz Item detials

--- app/src/main/java/sg/edu/nus/se26pt03/photolearn/fragment/QuizItemListFragment.java
+++ app/src/main/java/sg/edu/nus/se26pt03/photolearn/fragment/QuizItemListFragment.java
@@ -9,7 +9,9 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -19,54 +21,58 @@ import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import java.util.List;
+
+import sg.edu.nus.se26pt03.photolearn.BAL.Item;
 import sg.edu.nus.se26pt03.photolearn.BAL.LearningItem;
 import sg.edu.nus.se26pt03.photolearn.BAL.LearningTitle;
 import sg.edu.nus.se26pt03.photolearn.BAL.QuizItem;
 import sg.edu.nus.se26pt03.photolearn.BAL.QuizTitle;
 import sg.edu.nus.se26pt03.photolearn.BAL.Title;
+import sg.edu.nus.se26pt03.photolearn.BAL.User;
 import sg.edu.nus.se26pt03.photolearn.R;
 import sg.edu.nus.se26pt03.photolearn.adapter.ItemFragmentPageAdapter;
 import sg.edu.nus.se26pt03.photolearn.adapter.QuizItemFragmentPageAdapter;
+import sg.edu.nus.se26pt03.photolearn.application.App;
 import sg.edu.nus.se26pt03.photolearn.enums.AccessMode;
 import sg.edu.nus.se26pt03.photolearn.enums.UserRole;
+import sg.edu.nus.se26pt03.photolearn.service.ServiceCallback;
 import sg.edu.nus.se26pt03.photolearn.utility.ConstHelper;
 
 
 /**
  * Created by MyatMin on 08/3/18.
  */
-public class QuizItemListFragment extends BaseFragment {
+public class QuizItemListFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
 
-    public static String SharedPreferences_Access_Mode= "ACCESSMODE";
-    private ViewPager mPager;
-    private PagerAdapter mPagerAdapter;
+    public static String SharedPreferences_Access_Mode = "ACCESSMODE";
 
-    private TextView tvEmpty;
-    private FloatingActionButton Add;
-    private Dialog dialog;
+
     private int role;
     private int mode;
     private String sessionId;
-    private QuizTitle quizTitle;
     private String userId;
-    private ImageView popupimagebutton;
+    private List<Item> quizItemList = null;
 
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_learning_item_list, container, false);
-//    }
+
+    private Dialog dialog;
+    private ImageView popupimagebutton;
+    private QuizTitle quizTitle;
+    private SwipeRefreshLayout srf_quizItemList;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+    private TextView tvEmpty;
+    private FloatingActionButton Add;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View view= inflater.inflate(R.layout.fragment_learning_item_list, container, false);
-      //  super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_quiz_item_list, container, false);
+        //  super.onCreateView(inflater, container, savedInstanceState);
         //setContentView(R.layout.activity_view_page_with_fragment);
         quizTitle = (QuizTitle) getArguments().getSerializable(ConstHelper.REF_QUIZ_TITLES);
-        sessionId = "1";
+       // sessionId = "1";
+        //titleId="-L88Kii8Oc5tSrTBxNaW";
         mode = PreferenceManager.getDefaultSharedPreferences(getContext()).getInt(ConstHelper.SharedPreferences_Access_Mode, AccessMode.toInt(AccessMode.EDIT));
         role = PreferenceManager.getDefaultSharedPreferences(getContext()).getInt(ConstHelper.SharedPreferences_User_Id, UserRole.toInt(UserRole.PARTICIPENT));
         userId = PreferenceManager.getDefaultSharedPreferences(getContext()).getString(ConstHelper.SharedPreferences_User_Id, "0");
@@ -77,53 +83,64 @@ public class QuizItemListFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setupViews();
         setupControls();
+       // setupViews();
+
 
     }
 
     private void setupControls() {
-        popupimagebutton.setVisibility((mode == AccessMode.toInt(AccessMode.EDIT) && role == UserRole.toInt(UserRole.PARTICIPENT)) ? View.VISIBLE : View.GONE);
-        Add.setVisibility((mode == AccessMode.toInt(AccessMode.EDIT) && role == UserRole.toInt(UserRole.PARTICIPENT)) ? View.VISIBLE : View.GONE);
+
+
+        tvEmpty = (TextView) getView().findViewById(R.id.tv_itemempty_value);
+        Add = (FloatingActionButton) getView().findViewById(R.id.fab_addquizitem);
+        popupimagebutton = (ImageView) getView().findViewById(R.id.img_popupmenu);
+
         Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onCreate(new QuizItem(quizTitle),    null);
+                onCreate(new QuizItem(quizTitle), null);
             }
         });
         popupimagebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PopupMenu popupMenu= popupSetup(popupimagebutton);
+                PopupMenu popupMenu = popupSetup(popupimagebutton);
                 popupMenu.show();
+            }
+        });
+        mPager = (ViewPager) getView().findViewById(R.id.vp_quizitem);
+        mPagerAdapter = new QuizItemFragmentPageAdapter(getChildFragmentManager(), quizTitle,this.quizItemList);
+        mPager.setAdapter(mPagerAdapter);
+
+        srf_quizItemList =  getView().findViewById(R.id.srf_quizItemList);
+        srf_quizItemList.setOnRefreshListener(this);
+        srf_quizItemList.post(new Runnable() {
+            @Override
+            public void run() {
+                srf_quizItemList.setRefreshing(true);
+                loadList();
             }
         });
     }
 
     private void setupViews() {
-        tvEmpty = (TextView) getView().findViewById(R.id.tv_itemempty_value);
-        Add = (FloatingActionButton) getView().findViewById(R.id.fab_addlearningitem);
-        // Delete= (FloatingActionButton) findViewById(R.id.DeleteItemfloatingActionButton);
-        // Edit=(FloatingActionButton) findViewById(R.id.EditItemfloatingActionButton);
-
-        popupimagebutton = (ImageView) getView().findViewById(R.id.img_popupmenu);
-
-
-        mPager = (ViewPager) getView().findViewById(R.id.vp_quizitemlistt);
-        mPagerAdapter = new QuizItemFragmentPageAdapter(getFragmentManager(), quizTitle);
-        mPager.setAdapter(mPagerAdapter);
+        tvEmpty.setVisibility(mPagerAdapter.getCount() == 0 ? View.VISIBLE : View.INVISIBLE);
+        if (!UserRole.TRAINER.equals(this.role)) {
+            popupimagebutton.setVisibility(mPagerAdapter.getCount() > 0 ? View.VISIBLE : View.INVISIBLE);
+            Add.setVisibility(View.VISIBLE);
+        }
     }
 
-    public  PopupMenu popupSetup(ImageView imageView)
-    {
-        PopupMenu popupMenu=new PopupMenu(getContext(),imageView);
-        MenuInflater menuInflater=popupMenu.getMenuInflater();
-        menuInflater.inflate(R.menu.menu_learning_item,popupMenu.getMenu());
+    public PopupMenu popupSetup(ImageView imageView) {
+        PopupMenu popupMenu = new PopupMenu(getContext(), imageView);
+        MenuInflater menuInflater = popupMenu.getMenuInflater();
+        menuInflater.inflate(R.menu.menu_learning_item, popupMenu.getMenu());
         popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                boolean result=false;
-                switch (menuItem.getItemId()){
+                boolean result = false;
+                switch (menuItem.getItemId()) {
                     case R.id.menu_delete:
                         new AlertDialog.Builder(getContext())
                                 .setTitle("Title")
@@ -131,33 +148,69 @@ public class QuizItemListFragment extends BaseFragment {
                                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int whichButton) {
-                                       return;
-                                    }})
+                                        quizTitle.deleteItem(((QuizItemFragmentPageAdapter) mPagerAdapter).getQuizItemByPosition(mPager.getCurrentItem()).getId(), new ServiceCallback<Boolean>() {
+                                            @Override
+                                            public void onComplete(Boolean data) {
+                                                mPagerAdapter.notifyDataSetChanged();
+                                            }
+
+                                            @Override
+                                            public void onError(int code, String message, String details) {
+
+                                            }
+                                        });
+                                    }
+                                })
                                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
-                                       return;
+                                        return;
                                     }
                                 }).show();
-                        result=true;
+                        result = true;
                         break;
                     case R.id.menu_edit:
-                        onEdit(((ItemFragmentPageAdapter)mPagerAdapter).getLearningItemByPosition(mPager.getCurrentItem()), null);
-                        result=true;
+                        onEdit(((QuizItemFragmentPageAdapter) mPagerAdapter).getQuizItemByPosition(mPager.getCurrentItem()), null);
+                        result = true;
                         break;
                 }
                 return result;
             }
         });
-        return  popupMenu;
+        return popupMenu;
 
     }
+
+
+    private void loadList() {
+        srf_quizItemList.setRefreshing(true);
+        this.quizTitle.getItems(new ServiceCallback<List<Item>>() {
+
+            @Override
+            public void onComplete(List<Item> data) {
+
+                if (data != null) {
+                    if(quizItemList !=null )
+                        quizItemList.clear();
+                    QuizItemListFragment.this.quizItemList = data;
+                    ((QuizItemFragmentPageAdapter)mPagerAdapter).setQuizItemList(data);
+                    mPagerAdapter.notifyDataSetChanged();
+                    setupViews();
+                    srf_quizItemList.setRefreshing(false);
+                }
+            }
+            @Override
+            public void onError(int code, String message, String details) {
+                Log.w("ERROR", code + "-" + message + "-" + details);
+
+            }
+        });
+    }
+
+
     @Override
-    public void onResume() {
-        super.onResume();
-        tvEmpty.setVisibility(mPagerAdapter.getCount() == 0 ? View.VISIBLE : View.GONE);
-       // Delete.setVisibility(mPagerAdapter.getCount() == 0 ? View.INVISIBLE : View.VISIBLE);
-       // Edit.setVisibility(mPagerAdapter.getCount() == 0 ? View.INVISIBLE : View.VISIBLE);
+    public void onRefresh() {
+        loadList();
     }
 
 
