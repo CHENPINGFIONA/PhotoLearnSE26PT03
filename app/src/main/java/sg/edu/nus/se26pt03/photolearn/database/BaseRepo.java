e6a26d163b2df43a67bfdcd81d6023dd80cfdd1d
bkdpchaturvedi <37078558+bkdpchaturvedi@users.noreply.github.com>
2018-03-27 01:40:22 +0800

    commiting QUizITem

--- app/src/main/java/sg/edu/nus/se26pt03/photolearn/database/BaseRepo.java
+++ app/src/main/java/sg/edu/nus/se26pt03/photolearn/database/BaseRepo.java
@@ -10,9 +10,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import sg.edu.nus.se26pt03.photolearn.BAL.QuizTitle;
 import sg.edu.nus.se26pt03.photolearn.DAL.BaseDAO;
@@ -46,7 +51,7 @@ public class BaseRepo<T extends BaseDAO> implements AutoCloseable, IRepository<T
         t.setId(key);
         mDatabaseRef.child(key).setValue(t);
         mDatabaseRef.child(key).child("timestamp").setValue(ServerValue.TIMESTAMP);
-        mDatabaseRef.child(key).child("createdBy").setValue(App.currentUser.getId());
+        mDatabaseRef.child(key).child("createdBy").setValue(App.getCurrentUser().getId());
         mDatabaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -63,15 +68,28 @@ public class BaseRepo<T extends BaseDAO> implements AutoCloseable, IRepository<T
 
     @Override
     public void update(T t, final RepoCallback<Boolean> callback) {
+        Map<String, Object> map = new HashMap<>();
+        Class<?> c = t.getClass();
+        for (Method m: c.getDeclaredMethods()) {
+            if (m.getName().startsWith("get")) {
+                try {
+                    map.put(m.getName().replace("get" + m.getName().substring(3,4), m.getName().substring(3,4).toLowerCase()), m.invoke(t));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         DatabaseReference databaseReference = mDatabaseRef.child(t.getId());
         if (databaseReference == null) {
             callback.onComplete(false);
         } else {
-            databaseReference.setValue(t, new DatabaseReference.CompletionListener() {
+            databaseReference.updateChildren(map, new DatabaseReference.CompletionListener() {
                 @Override
                 public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
                     if (databaseError == null) {
-                        callback.onComplete(false);
+                        callback.onComplete(true);
                     } else {
                         callback.onError(databaseError);
                     }
@@ -153,24 +171,30 @@ public class BaseRepo<T extends BaseDAO> implements AutoCloseable, IRepository<T
 
     @Override
     public void getAllByKeyValue(String key, Object value, final RepoCallback<List<T>> callback) {
-        Query query = mDatabaseRef.orderByChild(key);
-        if (value instanceof Double) {
-            query = query.equalTo((Double) value);
-        }
-        else if (value instanceof String) {
-            query = query.equalTo((String) value);
-        }
-        else if (value instanceof Boolean) {
-            query = query.equalTo((Boolean) value);
-        }
+        String[] keys = key.split("\\.");
 
+        Query query = mDatabaseRef.orderByChild(keys[0]);
+        if (keys.length == 1) {
+            if (value instanceof Double) {
+                query = query.equalTo((Double) value);
+            } else if (value instanceof String) {
+                query = query.equalTo((String) value);
+            } else if (value instanceof Boolean) {
+                query = query.equalTo((Boolean) value);
+            }
+        }
         query.addValueEventListener(
             new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     List<T> result = new ArrayList<>();
                     for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
-                        result.add(getValue(childDataSnapshot));
+                        if (keys.length == 1) result.add(getValue(childDataSnapshot));
+                        else {
+                            if (isValid(childDataSnapshot,key, value)) {
+                                result.add(getValue(childDataSnapshot));
+                            }
+                        }
                     }
                     callback.onComplete(result);
                 }
@@ -182,12 +206,48 @@ public class BaseRepo<T extends BaseDAO> implements AutoCloseable, IRepository<T
             });
     }
 
+    private boolean isValid(DataSnapshot dataSnapshot, String key, Object value) {
+        String[] keys = key.split("\\.");
+        if (keys.length == 1) {
+            return dataSnapshot.getKey().equals(keys[0]) && dataSnapshot.getValue() == value;
+        }
+        for(DataSnapshot childDataSnapshot: dataSnapshot.child(keys[0]).getChildren()) {
+            String childKey = key.substring(key.indexOf(".") + 1);
+            if (isValid(childDataSnapshot, childKey, value )) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     protected T getValue(DataSnapshot dataSnapshot) {
         T t = dataSnapshot.getValue(tClass);
         if (t != null) {
             t.setId(dataSnapshot.getKey());
         }
         return t;
+    }
+
+
+    @Override
+    public void setValueByKey(String key, Object value, final RepoCallback<Boolean> callback) {
+        String[] keys = key.split("\\.");
+        DatabaseReference databaseReference = mDatabaseRef.child(keys[0]);
+        for (int i = 1; i < keys.length; i++) {
+            databaseReference = databaseReference.child(keys[i]);
+        }
+        databaseReference.setValue(value);
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                callback.onComplete(true);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                callback.onError(databaseError);
+            }
+        });
     }
 
 }
