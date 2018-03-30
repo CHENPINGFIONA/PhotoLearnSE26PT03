7aee6c9c2fd9f26b758aa330168ac6d1c32542f8
chenpingfiona <chen_ping2010@hotmail.com>
2018-03-30 18:01:20 +0800

    quiz title and learning title unit test

--- app/src/test/java/sg/edu/nus/se26pt03/photolearn/QuizTitleUnitTest.java
+++ app/src/test/java/sg/edu/nus/se26pt03/photolearn/QuizTitleUnitTest.java
@@ -1,71 +1,81 @@
 package sg.edu.nus.se26pt03.photolearn;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
-import java.util.List;
+import java.util.Date;
 
-import sg.edu.nus.se26pt03.photolearn.BAL.QuizItem;
+import sg.edu.nus.se26pt03.photolearn.BAL.LearningTitle;
 import sg.edu.nus.se26pt03.photolearn.BAL.QuizTitle;
+import sg.edu.nus.se26pt03.photolearn.service.BaseService;
+import sg.edu.nus.se26pt03.photolearn.utility.BindingHelper;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by chen ping on 22/3/2018.
  */
 
-public class QuizTitleUnitTest {
-    private QuizTitle quizTitle =null;
-    private List<QuizItem> quizItems;
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(JUnit4.class)
+@PrepareForTest({FirebaseDatabase.class, BindingHelper.class})
 
-    public QuizTitleUnitTest() {
-        SetInitialValues();
+public class QuizTitleUnitTest {
+    private QuizTitle title;
+    private DatabaseReference mockedDatabaseReference;
+    private BindingHelper mockBindingHelper;
+    private BaseService mocKbaseService;
+
+    @Before
+    public void setUp() throws Exception {
+        mockedDatabaseReference = Mockito.mock(DatabaseReference.class);
+        FirebaseDatabase mockedFirebaseDatabase = Mockito.mock(FirebaseDatabase.class);
+        when(mockedFirebaseDatabase.getReference()).thenReturn(mockedDatabaseReference);
+        PowerMockito.mockStatic(FirebaseDatabase.class);
+        when(FirebaseDatabase.getInstance()).thenReturn(mockedFirebaseDatabase);
+
+        title = new QuizTitle();
+        title.setTitle("Test title");
+        title.setCreatedBy("chen ping");
+        title.setId("TestTitle123");
+        title.setTimestamp(new Date());
+
+        PowerMockito.mockStatic(BindingHelper.class);
     }
 
-    private void SetInitialValues() {
+    @After
+    public void tearDown() {
+        title = null;
+    }
 
+    @Test
+    public void Assert_Copy() {
+        QuizTitle testTitle = new QuizTitle();
+        testTitle.setTitle("Test title");
+        testTitle.setCreatedBy("chen ping");
+        testTitle.setId("TestTitle123");
+
+        title.copy(testTitle);
+
+        assertEquals(testTitle.getId(), title.getId());
+        assertEquals(testTitle.getCreatedBy(), title.getCreatedBy());
+        assertEquals(testTitle.getTitle(), title.getTitle());
     }
 
     @Test
     public void Assert_GetQuizScore() throws Exception {
-
-    }
-
-    @Test
-    public void Assert_GetPreviousItem() throws Exception {
-
-    }
-
-    @Test
-    public void Assert_GetNextItem() throws Exception {
-
-    }
-
-    @Test
-    public void Assert_GetCurrentIteme() throws Exception {
-
-    }
-
-    @Test
-    public void Assert_RemoveAllAnswer() throws Exception {
-
-    }
-
-    @Test
-    public void Assert_CreateItem() throws Exception {
-
-
-    }
-
-    @Test
-    public void Assert_UpdateItem() throws Exception {
-
-    }
-
-    @Test
-    public void Assert_DeleteItem() throws Exception {
-
-    }
-
-    @Test
-    public void Assert_GetItems() throws Exception {
-
+        assertEquals(title.getQuizScore(1), 0);
     }
 }
